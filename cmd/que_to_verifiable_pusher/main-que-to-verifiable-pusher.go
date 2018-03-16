6d7efc2e40da7499c29523b8abbcce5ab1b0b83b
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-03-16 12:10:35 +1100

    Intermediate checkin

--- cmd/que_to_verifiable_pusher/main-que-to-verifiable-pusher.go
+++ cmd/que_to_verifiable_pusher/main-que-to-verifiable-pusher.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,11 +13,17 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/benlaurie/objecthash/go/objecthash"
+	que "github.com/bgentry/que-go"
+	ct "github.com/google/certificate-transparency-go"
+	"github.com/google/certificate-transparency-go/client"
+	"github.com/google/certificate-transparency-go/jsonclient"
+	"github.com/google/certificate-transparency-go/tls"
+	uuid "github.com/satori/go.uuid"
 
-	"github.com/govau/verifiable-log/pb"
 	"github.com/jackc/pgx"
 )
 
@@ -136,11 +145,181 @@ curl -H "Content-Type: application/json" -d "$D" -H "Authorization: ${CKAN_KEY}"
 type logAddHandler struct {
 	Server string
 	APIKey string
+
+	verifierMutex sync.RWMutex
+	verifiers     map[string]*ct.SignatureVerifier
+
+	logClientMutex sync.RWMutex
+	logClients     map[string]*client.LogClient
+
+	publicKeyMutex sync.RWMutex
+	publicKeyDERs  map[string][]byte
+}
+
+// Table name must already be canonical
+func (h *logAddHandler) baseURLForLog(canonTable string) string {
+	return fmt.Sprintf("%s/dataset/%s", h.Server, canonTable)
+}
+
+// Table name must already be canonical
+func (h *logAddHandler) getLogClient(canonTable string) (*client.LogClient, error) {
+	var rv *client.LogClient
+
+	h.logClientMutex.RLock()
+	if h.logClients != nil {
+		rv = h.logClients[canonTable]
+	}
+	h.logClientMutex.RUnlock()
+
+	if rv != nil {
+		return rv, nil
+	}
+
+	der, err := h.getPublicKeyDER(canonTable)
+	if err != nil {
+		return nil, err
+	}
+
+	rv, err = client.New(h.baseURLForLog(canonTable), http.DefaultClient, jsonclient.Options{
+		PublicKeyDER: der,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	h.logClientMutex.Lock()
+	defer h.logClientMutex.Unlock()
+
+	if h.logClients == nil {
+		h.logClients = make(map[string]*client.LogClient)
+	}
+	h.logClients[canonTable] = rv
+
+	return rv, nil
+}
+
+// Table name must already be canonical
+func (h *logAddHandler) getVerifier(canonTable string) (*ct.SignatureVerifier, error) {
+	var rv *ct.SignatureVerifier
+
+	h.verifierMutex.RLock()
+	if h.verifiers != nil {
+		rv = h.verifiers[canonTable]
+	}
+	h.verifierMutex.RUnlock()
+
+	if rv != nil {
+		return rv, nil
+	}
+
+	der, err := h.getPublicKeyDER(canonTable)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey, err := x509.ParsePKIXPublicKey(der)
+	if err != nil {
+		return nil, err
+	}
+
+	rv, err = ct.NewSignatureVerifier(pubKey)
+	if err != nil {
+		return nil, err
+	}
+
+	h.verifierMutex.Lock()
+	defer h.verifierMutex.Unlock()
+
+	if h.verifiers == nil {
+		h.verifiers = make(map[string]*ct.SignatureVerifier)
+	}
+	h.verifiers[canonTable] = rv
+
+	return rv, nil
+}
+
+// Table name must already be canonical
+func (h *logAddHandler) getPublicKeyDER(canonTable string) ([]byte, error) {
+	var rv []byte
+	h.publicKeyMutex.RLock()
+	if h.publicKeyDERs != nil {
+		rv = h.publicKeyDERs[canonTable]
+	}
+	h.publicKeyMutex.RUnlock()
+
+	if rv != nil {
+		return rv, nil
+	}
+
+	// Go fetch it, grab a mutex so we don't slam servers
+	h.publicKeyMutex.Lock()
+	defer h.publicKeyMutex.Unlock()
+
+	if h.publicKeyDERs == nil {
+		h.publicKeyDERs = make(map[string][]byte)
+	}
+
+	// One last check, since we now have a write mutex
+	rv = h.publicKeyDERs[canonTable]
+	if rv != nil {
+		return rv, nil
+	}
+
+	resp, err := http.Get(h.baseURLForLog(canonTable) + "/metadata")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("bad http status code fetching log metadata")
+	}
+
+	var md MetadataResponse
+	err = json.NewDecoder(resp.Body).Decode(&md)
+	if err != nil {
+		return nil, err
+	}
+
+	h.publicKeyDERs[canonTable] = md.Key
+	return md.Key, nil
+}
+
+// MetadataResponse is a subset of a log as defined at: https://www.gstatic.com/ct/log_list/log_list_schema.json
+type MetadataResponse struct {
+	Key []byte `json:"key"`
+}
+
+// table name must already be canonical
+func (h *logAddHandler) verifyIt(table, sctBase64 string, hash []byte) error {
+	curBytes, err := base64.StdEncoding.DecodeString(sctBase64)
+	if err != nil {
+		return err
+	}
+	var sct ct.SignedCertificateTimestamp
+	remaining, err := tls.Unmarshal(curBytes, &sct)
+	if err != nil {
+		return err
+	}
+	if len(remaining) != 0 {
+		return errors.New("trailing bytes")
+	}
+
+	verifier, err := h.getVerifier(table)
+	if err != nil {
+		return err
+	}
+
+	err = verifier.VerifySCTSignature(sct, ct.LogEntry{Leaf: *ct.CreateObjectHashMerkleTreeLeaf(hash, sct.Timestamp)})
+	if err != nil {
+		return err
+	}
+
+	// All good
+	return nil
 }
 
 func (h *logAddHandler) addToVerifiableLog(job *que.Job) error {
-	client := pb.NewQueHandlerProtobufClient(h.Server, http.DefaultClient)
-
 	var jd struct {
 		Table string                 `json:"table"`
 		Data  map[string]interface{} `json:"data"`
@@ -148,6 +327,18 @@ func (h *logAddHandler) addToVerifiableLog(job *que.Job) error {
 	err := json.Unmarshal(job.Args, &jd)
 	if err != nil {
 		return err
+	}
+
+	// Make sure table is a UUID to prevent us from making an inadvertent call to the wrong path
+	u, err := uuid.FromString(jd.Table)
+	if err != nil {
+		return err
+	}
+	canonTable := u.String()
+
+	id, ok := jd.Data["_id"].(int)
+	if !ok {
+		return errors.New("no _id found for object")
 	}
 
 	dataToSend := make(map[string]interface{})
@@ -160,7 +351,7 @@ func (h *logAddHandler) addToVerifiableLog(job *que.Job) error {
 		if k == "signed_certificate_timestamp" {
 			continue
 		}
-		// Ignore null values so that columns can be added over time
+		// Ignore null values so that columns can be added over time, without affecting the signature
 		if v == nil {
 			continue
 		}
@@ -172,33 +363,33 @@ func (h *logAddHandler) addToVerifiableLog(job *que.Job) error {
 		return err
 	}
 
-	bb, err := json.Marshal(dataToSend)
-	if err != nil {
-		return err
-	}
-
 	currentSCT, _ := jd.Data["signed_certificate_timestamp"].(string)
-	id, _ := jd.Data["_id"].(int)
-	// TODO - verify if existing SCT matches data, and terminate early if so
-	// In fact, given that we'll trigger ourselves, we better do that!
+	if currentSCT != "" {
+		err = h.verifyIt(canonTable, currentSCT, oh[:])
+		if err != nil {
+			return err
+		}
 
-	resp, err := client.AddToLog(context.Background(), &pb.AddToLogReq{
-		Auth: &pb.Authorization{
-			ApiKey: h.APIKey,
-		},
-		Table: jd.Table,
-		Data:  string(bb),
-	})
-	if err != nil {
-		return err
-	}
-
-	if resp.SignedCertificateTimestamp == currentSCT {
-		// We're done, no work required, return
+		// If we already have a valid one, stop now
 		return nil
 	}
 
-	_, err = job.Conn().Exec(fmt.Sprintf(`UPDATE "%s" SET signed_certificate_timestamp = $1 WHERE _id = $2`, jd.Table), resp.SignedCertificateTimestamp, id)
+	logClient, err := h.getLogClient(canonTable)
+	if err != nil {
+		return err
+	}
+
+	sct, err := logClient.AddObjectHash(context.Background(), oh[:], dataToSend)
+	if err != nil {
+		return err
+	}
+
+	tlsEncode, err := tls.Marshal(sct)
+	if err != nil {
+		return err
+	}
+
+	_, err = job.Conn().Exec(fmt.Sprintf(`UPDATE "%s" SET signed_certificate_timestamp = $1 WHERE _id = $2`, jd.Table), base64.StdEncoding.EncodeToString(tlsEncode), id)
 	if err != nil {
 		return err
 	}
