ea4299ceec35f06dd6a0f81b8ac84a4457c5d622
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-03-23 15:56:44 +1100

    dep ensure and go generate

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
 //go:generate protoc -Ipb pb/metadata.proto --go_out=pb
-//go:generate go-bindata -debug -pkg assets -o assets/assets.go assets/static/
+//go:generate go-bindata -pkg assets -o assets/assets.go assets/static/
 
 package verifiablelogs
