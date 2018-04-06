f8c88826279c17e7aa4f405dd0a739d7cb996723
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-04-06 12:09:19 +1000

    Update to latest library

--- cmd/submit-from-external/main-submit-from-external.go
+++ cmd/submit-from-external/main-submit-from-external.go
@@ -3,32 +3,37 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"os/signal"
+	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/bgentry/que-go"
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 	"github.com/jackc/pgx"
 
-	"github.com/govau/certwatch/db"
-	"github.com/govau/certwatch/jobs"
 	"github.com/govau/cf-common/env"
+	"github.com/govau/cf-common/jobs"
 	"github.com/govau/verifiable-logs/generalisedtransparency"
-)
-
-const (
-	WorkerCount = 1
 )
 
 type resource struct {
 	Resource string `json:"resource"`
+}
+
+type fetchEntriesArgs struct {
+	Resource string `json:"resource"`
+
+	// Start is inclusive
+	Start int `json:"start"`
+
+	// End is inclusive
+	End int `json:"end"`
 }
 
 type submitRecord struct {
@@ -46,42 +51,8 @@ func main() {
 		env.WithUPSLookup(app, "submit-external-ups"),
 	)
 
-	pgxPool, err := db.GetPGXPool(WorkerCount * 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	baseURL := envLookup.String("CKAN_BASE_URL", "https://data.gov.au")
 	resourceIDs := strings.Split(envLookup.String("CKAN_RESOURCE_ID", ""), ",")
-
-	qc := que.NewClient(pgxPool)
-
-	tx, err := pgxPool.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tx.Rollback()
-
-	for _, rid := range resourceIDs {
-		bb, err := json.Marshal(&resource{
-			Resource: rid,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = qc.EnqueueInTx(&que.Job{
-			Type:  "fetch_entry_metadata",
-			Args:  bb,
-			RunAt: time.Now(),
-		}, tx)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	err = tx.Commit()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	tableValidator, err := generalisedtransparency.CreateNamedValidator("whitelist", strings.Join(resourceIDs, ","))
 	if err != nil {
@@ -94,141 +65,271 @@ func main() {
 		TableNameValidator: tableValidator,
 	}
 
-	workers := que.NewWorkerPool(qc, que.WorkMap{
-		"fetch_entry_metadata": (&jobs.JobFuncWrapper{
-			QC:     qc,
-			Logger: log.New(os.Stderr, "", log.LstdFlags),
-			F: func(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx) error {
-				var r resource
-				err := json.Unmarshal(job.Args, &r)
-				if err != nil {
-					return err
-				}
-
-				tName, err := tableValidator.ValidateAndCanonicaliseTableName(r.Resource)
-				if err != nil {
-					return err
-				}
-
-				var lastID int
-				err = tx.QueryRow(`SELECT last_id FROM processed_ids WHERE resource = $1 FOR UPDATE`, tName).Scan(&lastID)
-				switch err {
-				case nil:
-					// continue
-				case pgx.ErrNoRows:
-					_, err = tx.Exec("INSERT INTO processed_ids(resource, last_id) VALUES ($1, $2)", tName, 0)
-					if err != nil {
-						return err
-					}
-					return jobs.ErrImmediateReschedule
-				default:
-					return err
-				}
-
-				// Go out and search
-				resp, err := http.Get(baseURL + "/api/3/action/datastore_search_sql?" + (&url.Values{
-					"sql": []string{fmt.Sprintf(`SELECT * FROM "%s" WHERE _id > %d ORDER BY _id LIMIT 100`, tName, lastID)},
-				}).Encode())
-				if err != nil {
-					return err
-				}
-				defer resp.Body.Close()
-
-				if resp.StatusCode != http.StatusOK {
-					return fmt.Errorf("bad status code: %d", resp.StatusCode)
-				}
-
-				var jsonResp struct {
-					Result struct {
-						Records []map[string]interface{} `json:"records"`
-					} `json:"result"`
-				}
-				err = json.NewDecoder(resp.Body).Decode(&jsonResp)
-				if err != nil {
-					return err
-				}
-
-				if len(jsonResp.Result.Records) == 0 {
-					return nil
-				}
-
-				for _, dataToSubmit := range jsonResp.Result.Records {
-					id, err := generalisedtransparency.JSONIntID(dataToSubmit["_id"])
-					if err != nil {
-						return err
-					}
-
-					bb, err := json.Marshal(&submitRecord{
-						Table: tName,
-						Data:  dataToSubmit,
-					})
-					if err != nil {
-						return err
-					}
-
-					err = qc.EnqueueInTx(&que.Job{
-						Type: "update_sct",
-						Args: bb,
-					}, tx)
-					if err != nil {
-						return err
-					}
-
-					// we're in ASC order
-					lastID = id
-				}
-
-				_, err = tx.Exec(`UPDATE processed_ids SET last_id = $2 WHERE resource = $1`, tName, lastID)
-				if err != nil {
-					return err
-				}
-
-				return tx.Commit()
-			},
-			Singleton: true,
-			Duration:  time.Minute * 5,
-		}).Run,
-		"update_sct": (&jobs.JobFuncWrapper{
-			QC:     qc,
-			Logger: log.New(os.Stderr, "", log.LstdFlags),
-			F: func(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx) error {
-				var jd submitRecord
-				err := json.Unmarshal(job.Args, &jd)
-				if err != nil {
-					return err
-				}
-				return logSubmitter.SubmitToLogAndUpdateRecord(context.Background(), jd.Table, jd.Data, nil)
-			},
-		}).Run,
-	}, WorkerCount)
-
-	// Prepare a shutdown function
-	shutdown := func() {
-		workers.Shutdown()
-		pgxPool.Close()
+	workerCount, err := strconv.Atoi(envLookup.String("QUE_WORKERS", "2"))
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Normal exit
-	defer shutdown()
+	log.Fatal((&jobs.Handler{
+		PGXConnConfig: jobs.MustPGXConfigFromCloudFoundry(),
+		WorkerCount:   workerCount,
+		InitSQL: `
+			CREATE TABLE IF NOT EXISTS processed_ids (
+				resource   text      PRIMARY KEY,
+				last_id    bigint    NOT NULL DEFAULT 0
+			);
+		`,
+		OnStart: func(qc *que.Client, pool *pgx.ConnPool, logger *log.Logger) error {
+			err := bootstrapJobs(qc, pool, resourceIDs)
+			if err != nil {
+				return err
+			}
+			go log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "HEALTHY")
+			})))
+			return nil
+		},
+		WorkerMap: map[string]*jobs.JobConfig{
+			"update_sct": &jobs.JobConfig{
+				F: func(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx) error {
+					var jd submitRecord
+					err := json.Unmarshal(job.Args, &jd)
+					if err != nil {
+						return err
+					}
+					return logSubmitter.SubmitToLogAndUpdateRecord(context.Background(), jd.Table, jd.Data, nil)
+				},
+			},
 
-	// Or via signals
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-	signal.Notify(sigCh, syscall.SIGTERM)
-	go func() {
-		sig := <-sigCh
-		log.Printf("Received %v, starting shutdown...", sig)
-		shutdown()
-		log.Println("Shutdown complete")
-		os.Exit(0)
-	}()
+			"fetch_entry_metadata": &jobs.JobConfig{
+				F: func(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx) error {
+					return fetchEntryMetadata(qc, logger, job, tx, tableValidator, baseURL)
+				},
+				Singleton: true,
+				Duration:  time.Minute * 5,
+			},
 
-	go workers.Start()
+			"fetch_entries": &jobs.JobConfig{
+				F: func(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx) error {
+					return fetchEntries(qc, logger, job, tx, tableValidator, baseURL)
+				},
+			},
+		},
+	}).WorkForever())
+}
 
-	log.Println("Started up... waiting for ctrl-C.")
+func bootstrapJobs(qc *que.Client, pool *pgx.ConnPool, resourceIDs []string) error {
+	tx, err := pool.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Up and away.")
+	for _, rid := range resourceIDs {
+		bb, err := json.Marshal(&resource{
+			Resource: rid,
+		})
+		if err != nil {
+			return err
+		}
+		err = qc.EnqueueInTx(&que.Job{
+			Type: "fetch_entry_metadata",
+			Args: bb,
+		}, tx)
+		if err != nil {
+			return err
+		}
+	}
+	return tx.Commit()
+}
+
+const (
+	maxAtOnce = 100
+)
+
+func fetchEntries(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx, tableValidator generalisedtransparency.TableNameValidator, baseURL string) error {
+	var jd fetchEntriesArgs
+	err := json.Unmarshal(job.Args, &jd)
+	if err != nil {
+		return err
+	}
+
+	tName, err := tableValidator.ValidateAndCanonicaliseTableName(jd.Resource)
+	if err != nil {
+		return err
+	}
+
+	desiredEnd := jd.End
+	if desiredEnd > (jd.Start + maxAtOnce) {
+		desiredEnd = jd.Start + maxAtOnce
+	}
+
+	// Schedule more jobs if needed
+	if desiredEnd < jd.End {
+		midpoint := (jd.End + desiredEnd) / 2
+
+		nextA := &fetchEntriesArgs{Resource: jd.Resource, Start: desiredEnd + 1, End: midpoint}
+		if nextA.Start <= nextA.End && nextA.End <= jd.End {
+			bb, err := json.Marshal(nextA)
+			if err != nil {
+				return err
+			}
+			err = qc.EnqueueInTx(&que.Job{
+				Type: "fetch_entries",
+				Args: bb,
+			}, tx)
+			if err != nil {
+				return err
+			}
+		}
+
+		nextB := &fetchEntriesArgs{Resource: jd.Resource, Start: midpoint + 1, End: jd.End}
+		if nextB.Start <= nextB.End {
+			bb, err := json.Marshal(nextB)
+			if err != nil {
+				return err
+			}
+			err = qc.EnqueueInTx(&que.Job{
+				Type: "fetch_entries",
+				Args: bb,
+			}, tx)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+
+	// Go out and search
+	resp, err := http.Get(baseURL + "/api/3/action/datastore_search_sql?" + (&url.Values{
+		"sql": []string{fmt.Sprintf(`SELECT * FROM "%s" WHERE _id >= %d AND _id <= %d`, tName, jd.Start, desiredEnd)},
+	}).Encode())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status code: %d", resp.StatusCode)
+	}
+
+	var jsonResp struct {
+		Result struct {
+			Records []map[string]interface{} `json:"records"`
+		} `json:"result"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return err
+	}
+
+	for _, dataToSubmit := range jsonResp.Result.Records {
+		bb, err := json.Marshal(&submitRecord{
+			Table: tName,
+			Data:  dataToSubmit,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = qc.EnqueueInTx(&que.Job{
+			Type: "update_sct",
+			Args: bb,
+		}, tx)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Please commit and save
+	return nil
+}
+
+func fetchEntryMetadata(qc *que.Client, logger *log.Logger, job *que.Job, tx *pgx.Tx, tableValidator generalisedtransparency.TableNameValidator, baseURL string) error {
+	var r resource
+	err := json.Unmarshal(job.Args, &r)
+	if err != nil {
+		return err
+	}
+
+	tName, err := tableValidator.ValidateAndCanonicaliseTableName(r.Resource)
+	if err != nil {
+		return err
+	}
+
+	var lastID int
+	err = tx.QueryRow(`SELECT last_id FROM processed_ids WHERE resource = $1 FOR UPDATE`, tName).Scan(&lastID)
+	switch err {
+	case nil:
+		// continue
+	case pgx.ErrNoRows:
+		_, err = tx.Exec("INSERT INTO processed_ids(resource, last_id) VALUES ($1, $2)", tName, 0)
+		if err != nil {
+			return err
+		}
+		return jobs.ErrImmediateReschedule
+	default:
+		return err
+	}
+
+	// Go out and search
+	resp, err := http.Get(baseURL + "/api/3/action/datastore_search_sql?" + (&url.Values{
+		"sql": []string{fmt.Sprintf(`SELECT MAX(_id) FROM "%s"`, tName)},
+	}).Encode())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status code: %d", resp.StatusCode)
+	}
+
+	var jsonResp struct {
+		Result struct {
+			Records []struct {
+				ID int `json:"_id"`
+			} `json:"records"`
+		} `json:"result"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return err
+	}
+
+	if len(jsonResp.Result.Records) == 0 {
+		return errors.New("unexpectedly got no results fetching max id")
+	}
+
+	maxID := jsonResp.Result.Records[0].ID
+
+	if maxID <= lastID {
+		// our work here is done
+		return nil
+	}
+
+	bb, err := json.Marshal(&fetchEntriesArgs{
+		Resource: tName,
+		Start:    lastID + 1,
+		End:      maxID,
 	})
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+	if err != nil {
+		return err
+	}
+
+	err = qc.EnqueueInTx(&que.Job{
+		Type: "fetch_entries",
+		Args: bb,
+	}, tx)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(`UPDATE processed_ids SET last_id = $2 WHERE resource = $1`, tName, maxID)
+	if err != nil {
+		return err
+	}
+
+	// Please commit and save
+	return nil
 }
